219060893fbdc83ebfaa37a4067c1c2bf54fdfa5
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-05-25 15:43:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let text = document.getElementById('text');
+let delay = document.getElementById('delay');
+let btn = document.getElementById('btn');
+let output = document.getElementById('output');
+
+async function display(){
+	const message = text.value;
+	const delayVal = delay.value;
+	await new Promise((resolve) => setTimeout(resolve, delayVal));
+	output.innerText = message;
+}
+
+btn.addEventListener('click', display);
